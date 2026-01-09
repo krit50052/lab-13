c200d85b8934f2efd11fd8efa3db03930e5801f9
krit <krittapasboonruang@gmail.com>
2026-01-09 14:39:44 +0700

    lab13_2 completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,41 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void updateImage(bool image[][M], int s, int x, int y) {
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < M; j++) {
+           
+            double distance = sqrt(pow(i - x, 2) + pow(j - y, 2));
+            
+            if(distance <= s - 1) {
+                image[i][j] = true;
+            }
+        }
+    }
+}
+
+void showImage(const bool image[][M]) {
+    
+    for(int j = 0; j < M; j++) {
+        cout << "-";
+    }
+    cout << " \n";
+    
+    for(int i = 0; i < N; i++) {
+        cout << "|";  
+        for(int j = 0; j < M; j++) {
+            if(image[i][j]) {
+                cout << "*";  
+            } else {
+                cout << " "; 
+            }
+        }
+        cout << "|\n"; 
+    }
+    
+    for(int j = 0; j < M; j++) {
+        cout << "-";
+    }
+    cout << " \n";
+}
